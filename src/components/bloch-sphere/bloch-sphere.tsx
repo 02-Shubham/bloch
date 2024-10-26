5e17bf209d18b41e8dec771d750a08ddbee86174
dolnaaa <matedolnik@gmail.com>
2024-10-26 22:00:42 +0200

    fix: pan fix

--- src/components/bloch-sphere/bloch-sphere.tsx
+++ src/components/bloch-sphere/bloch-sphere.tsx
@@ -53,7 +53,7 @@ export const BlochSphere: React.FC<BlochSphereProps> = ({
   return (
     <Canvas camera={{ position: [3, 3, 3], fov: 35 }}>
       {/* Controls for panning and rotating */}
-      <OrbitControls enableZoom={true} zoomToCursor={false} />
+      <OrbitControls enableZoom={true} zoomToCursor={false} enablePan={false} />
 
       {/* Light sources */}
       <ambientLight intensity={0.5} />
