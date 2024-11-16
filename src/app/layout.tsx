b109b890246a0b70761f5197ea7b6ef6c7358ee9
dolnaaa <matedolnik@gmail.com>
2024-11-16 21:34:48 +0100

    chore: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { AppContextProvider } from "@/state/app-context";
 export const metadata: Metadata = {
   title: "Bloch sphere simulator",
   description: "This is a bloch sphere simulator for an uni project",
+  icons: {
+    icon: "/icon.png",
+  },
 };
 
 export default function RootLayout({
